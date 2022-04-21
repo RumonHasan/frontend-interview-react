70f11785432ec0942913d613496591d660184a55
hasan rumon <hasanrumon@hasans-MacBook-Pro.local>
2022-04-21 17:00:29 +0900

    final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React,{useEffect, useState} from 'react';
 import './style.css';
-import { test_items } from './extras';
+import { test_items, submissions } from './extras';
 
 const App = () => {
   const questions = test_items;
   const [questionObject, setQuestionObject] = useState({});
+  const [submissionsObject, setSubmissionsObject] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  let correctCounter = 0;
 
-  // to get questions by category
-  useEffect(()=>{
+    // to get questions by category
     const getQuestionsByCategory = ()=>{
         questions.forEach((question)=>{
             setQuestionObject((prevQuestionObject)=>{
@@ -27,14 +28,43 @@ const App = () => {
                 }
             })
         })
+}
+    // setting the submissions object
+    const getSubmissionsById = ()=>{
+        submissions.forEach((submission)=>{
+            setSubmissionsObject((prevSubmissionObject)=>{
+                return {
+                    ...prevSubmissionObject,
+                    [submission.questionId]: submission.status
+                }
+            })
+        })
     }
+
+  useEffect(()=>{
+    getSubmissionsById();
     getQuestionsByCategory();
   },[]);
 
-
   return (
     <div className='list-container'>
-
+        {Object.entries(questionObject).map((keyName, key)=>{
+            return (
+                <div key={key} className='category'>
+                    <h2>{keyName[0]}- {correctCounter}/{keyName[1].length}</h2>
+                    {keyName[1]?.map((question, index)=>{
+                        const statusClass = submissionsObject[question.id]?.toLowerCase()?.replace('_', '-');
+                        return (
+                            <div className={`question`} key={question.id}>
+                                <div className={`status ${statusClass}`}></div>
+                                {submissionsObject[question.id] === 'CORRECT' && correctCounter++}
+                                {question.name}
+                            </div>
+                        )
+                    })}
+                </div>
+            )
+        })}
     </div>
   )
 };
